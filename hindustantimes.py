be411f76d4406e3c136dea8ab6eeb254a46626d9
vikas.0694 <vikas.0694@gmail.com>
2018-11-06 16:06:33 +0530

    greendeck assignment of fetching the news in csv file

---
+++ hindustantimes.py
@@ -0,0 +1,31 @@
+import requests  
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup 
+
+def htnews(): 
+    url1='https://www.hindustantimes.com/latest-news'
+    url2='https://www.hindustantimes.com/top-news'
+    
+    web_response = requests.get(url1, url2) 
+    
+    if web_response.status_code==200:     
+        soup = BeautifulSoup(web_response.text, 'html.parser')	 
+        newslist = soup.find("ul", {"class":"latest-news-bx"}) 
+        with open("Latest_News.csv", 'w') as f:
+            writer = csv.writer(f, dialect='excel')
+            writer.writerow(['News', 'Link'])
+            for news in newslist.findAll("a"):
+                if news.text.startswith(" read more") or news.text.startswith("read more") :
+                    pass
+                else:
+                    writer.writerow(news)
+        f.close()
+        df = pd.read_csv('Latest_News.csv')
+        df.drop('Link', axis = 1, inplace = True)
+        df.to_csv('Latest_News.csv', index = False)
+        print("News Extracted from Hindustan Times has been Succcessfully imported to \n Latest_News.csv")
+    else: 
+        print("Webpage Error: Please try again...") 
+  
+htnews()
---
+++ readme.md
@@ -0,0 +1,12 @@
+Python script that can fetch latest news headlines from Hindustantimes Latest news
+
+Follow these simple steps to extract the news :-
+
+1. Open terminal
+2. Clone the repository:
+    **$ `git clone https://github.com/vikas0694/greendeck_assignment.git`**
+3. cd greendeck_assignment
+4. Pip3 install -r requirements.txt
+5. python3 hindustantimes.py
+
+**A csv file named Latest_news.csv will be generated on the current directory**
